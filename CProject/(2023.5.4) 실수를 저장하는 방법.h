62226e9ba9120c0037cb93f3688a9f65fb3d3c60
kwh0163 <dlawjdgid75@naver.com>
2023-05-10 21:49:46 +0900

    (2023.5.10)

---
+++ CProject/(2023.5.4) 실수를 저장하는 방법.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <stdio.h> // 기본 입출력 헤더 파일
+#include <math.h>
+
+struct Player
+{
+	int hp;
+	int attack;
+	int x;
+	int y;
+};
+
+struct Enemy
+{
+	int x;
+	int y;
+};
+
+int main()
+{
+#pragma region 구조체 포인터
+	//struct Player player;
+
+	//struct Player * ptrPlayer = NULL;
+
+	//ptrPlayer = &player;
+
+	//(*ptrPlayer).hp = 100;
+	//(*ptrPlayer).attack = 12.5f;
+
+	//printf("%d\n", (*ptrPlayer).hp);
+	//printf("%f\n", (*ptrPlayer).attack);
+
+	//ptrPlayer->hp = 250;
+	//ptrPlayer->attack = 22.75f;
+
+	//printf("%d\n", (*ptrPlayer).hp);
+	//printf("%f\n", (*ptrPlayer).attack);
+#pragma endregion
+
+#pragma region 두 점 사이의 거리
+
+	//// 제곱근 (sqrt)
+	//printf("루트 49 : %lf\n", sqrt(49));
+
+	//// 거듭제곱
+	//printf("2의 3승 : %lf\n", pow(2, 3));
+
+	//struct Player player1;
+	//struct Enemy enemy;
+
+	//player1.x = 0;
+	//player1.y = 0;
+
+	//enemy.x = 5;
+	//enemy.y = 7;
+	//
+	//float xDistance = player1.x - enemy.x;
+	//float yDistance = player1.y - enemy.y;
+
+	//float distance = sqrt(pow(xDistance, 2) + pow(yDistance, 2));
+
+	//if (distance >= 5)
+	//{
+	//	printf("위험한 상태가 아닙니다");
+	//}
+
+	//printf("%f", distance);
+
+#pragma endregion
+
+#pragma region 실수를 저장하는 방법
+
+	// 부동 소수점
+	// 소수점의 위치를 고정하지 않고 소수점의 위치를
+	// 나타내는 방법입니다.
+
+	// 고정 소수점
+	// 소수점의 위치를 고정하여 소수점의 위치를 나타내는 방법입니다.
+
+	// 12.375
+
+	// float 4(Byte)
+	//float fData = 1.11f;
+	//printf("fData의 값 : %.15f\n", fData);
+
+	//// double 8(Byte)
+	//double dData = 1.3f;
+	//printf("dData의 값 : %.10lf\n", dData);
+
+	//if (fData == 1.3f)
+	//{
+	//	printf("fData의 값과 1.3f의 값다.");
+	//}
+	//else
+	//{
+	//	printf("fData의 값과 1.3의 값이 같지 않다.");
+	//}
+
+#pragma endregion
+
+	return 0;
+}
--- CProject/Source.c
+++ CProject/Source.c
@@ -1,102 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h> // 기본 입출력 헤더 파일
-#include <math.h>
 
-struct Player
-{
-	int hp;
-	int attack;
-	int x;
-	int y;
-};
+#pragma region  자기 참조 구조체
+		// 자기 자신을 가리키는 포인터가
+		// 구조체의 멤버로 들어가 있는 구조체입니다.
 
-struct Enemy
+typedef struct Node
 {
-	int x;
-	int y;
-};
+	int data;
+	struct Node* next;
+}Node;
+
+#pragma endregion
 
 int main()
 {
-#pragma region 구조체 포인터
-	//struct Player player;
+#pragma region 자기 참조 구조체
 
-	//struct Player * ptrPlayer = NULL;
+	/*Node node1 = { 100, NULL };
+	Node node2 = { 200, NULL };
+	Node node3 = { 300, NULL };
 
-	//ptrPlayer = &player;
+	printf("node1의 data 값 : %d\n", node1.data);
+	printf("node2의 data 값 : %d\n", node2.data);
+	printf("node3의 data 값 : %d\n", node3.data);
 
-	//(*ptrPlayer).hp = 100;
-	//(*ptrPlayer).attack = 12.5f;
+	node1.next = &node2;
+	node2.next = &node3;
+	node3.next = NULL;
 
-	//printf("%d\n", (*ptrPlayer).hp);
-	//printf("%f\n", (*ptrPlayer).attack);
+	node1.next->data = 999;
 
-	//ptrPlayer->hp = 250;
-	//ptrPlayer->attack = 22.75f;
+	Node* currentPtr = &node1;
 
-	//printf("%d\n", (*ptrPlayer).hp);
-	//printf("%f\n", (*ptrPlayer).attack);
-#pragma endregion
+	while (currentPtr != NULL)
+	{
+		printf("data의 값 : %d\n", currentPtr->data);
 
-#pragma region 두 점 사이의 거리
-
-	//// 제곱근 (sqrt)
-	//printf("루트 49 : %lf\n", sqrt(49));
-
-	//// 거듭제곱
-	//printf("2의 3승 : %lf\n", pow(2, 3));
-
-	//struct Player player1;
-	//struct Enemy enemy;
-
-	//player1.x = 0;
-	//player1.y = 0;
-
-	//enemy.x = 5;
-	//enemy.y = 7;
-	//
-	//float xDistance = player1.x - enemy.x;
-	//float yDistance = player1.y - enemy.y;
-
-	//float distance = sqrt(pow(xDistance, 2) + pow(yDistance, 2));
-
-	//if (distance >= 5)
-	//{
-	//	printf("위험한 상태가 아닙니다");
-	//}
-
-	//printf("%f", distance);
+		currentPtr = currentPtr->next;
+	}*/
 
 #pragma endregion
 
-#pragma region 실수를 저장하는 방법
+#pragma region 삼항 연산자
+	//int data = 0;
 
-	// 부동 소수점
-	// 소수점의 위치를 고정하지 않고 소수점의 위치를
-	// 나타내는 방법입니다.
+	//	//  (조건) ? (참) : (거짓)
+	//data = 10 == 10 ? 100 : 445;
 
-	// 고정 소수점
-	// 소수점의 위치를 고정하여 소수점의 위치를 나타내는 방법입니다.
-
-	// 12.375
-	
-	// float 4(Byte)
-	//float fData = 1.11f;
-	//printf("fData의 값 : %.15f\n", fData);
-
-	//// double 8(Byte)
-	//double dData = 1.3f;
-	//printf("dData의 값 : %.10lf\n", dData);
-
-	//if (fData == 1.3f)
-	//{
-	//	printf("fData의 값과 1.3f의 값다.");
-	//}
-	//else
-	//{
-	//	printf("fData의 값과 1.3의 값이 같지 않다.");
-	//}
+	//printf("data의 값 : %d\n", data);
 
 #pragma endregion
+
+
+	char string[10];
+
+	scanf_s("%[^\n]s", string);
+
+	printf("%string의 값 : %s", string);
 
 
 
