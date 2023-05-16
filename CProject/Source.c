90929fa10827ff4bffd6f4e4cb298c59802aa7c6
kwh0163 <dlawjdgid75@naver.com>
2023-05-16 21:48:20 +0900

    2023.5.16 좌표 이동 함수

--- CProject/Source.c
+++ CProject/Source.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h> // 기본 입출력 헤더 파일	
 #include <conio.h>
 #include <Windows.h>
@@ -7,70 +9,93 @@
 #define RIGHT 77
 #define DOWN 80
 
+// 좌표 이동 함수
+void GotoXY(int x, int y)
+{
+	COORD position = { x, y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
+
+
 int main()
 {
-#pragma region _kbhit() 함수
+#pragma region 좌표 이동 함수
 
-	//char key = 0;
-
-	//while (1)
-	//{
-	//	//if (_kbhit()) // 키보드 입력을 확인하는 함수
+	//	int x = 0;
+	//	int y = 0;
+	//	int key = 0;
 	//	
-	//		key = _getch(); // 방향키 입력시 224 00이 들어오게 되므로 앞에 있는 값 224를 삭제합니다.
-
-	//		if (key == -32)
+	//	while(1)
+	//	{
+	//		GotoXY(x, y);
+	//		printf("★");
+	//	
+	//		if (_kbhit()) // 키보드 입력을 확인하는 함수
 	//		{
-	//			key = _getch();
+	//			key = _getch(); // 방향키 입력시 224 00이 들어오게 되므로 앞에 있는 값 224를 삭제합니다.
+	//	
+	//			if (key == -32)
+	//			{
+	//				key = _getch();
+	//			}
+	//	
+	//			switch (key)
+	//			{
+	//			case UP: if (y > 0) y--;
+	//				break;
+	//			case LEFT: if(x > 0) x--;
+	//				break;
+	//			case RIGHT: x++;
+	//				break;
+	//			case DOWN: y++;
+	//				break;
+	//			}
+	//	
+	//			system("cls");
 	//		}
-
-	//		switch (key)
-	//		{
-	//			case UP: printf("↑\n");
-	//				break;
-	//			case LEFT: printf("←\n");
-	//				break;
-	//			case RIGHT: printf("→\n");
-	//				break;
-	//			case DOWN: printf("↓\n");
-	//				break;
-	//			default: printf("다시 입력해주세요.\n");
-	//				break;
-	//		}
-
-	//		printf("다른 작업 중...\n");
-	//}
+	//	}
 
 #pragma endregion
 
-#pragma region GetAsyncKeyState() 함수
+#pragma region 문자열 길이 함수(strlen)
+	// 문자열의 크기를 반환하는 함수입니다.
 
-	while (1)
-	{
-		if (GetAsyncKeyState(VK_UP))
-		{
-			// 1000 (1 초) 
-			Sleep(100);
-			printf("↑\n");
-		}
-		if (GetAsyncKeyState(VK_LEFT))
-		{
-			Sleep(100);
-			printf("←\n");
-		}
-		if (GetAsyncKeyState(VK_RIGHT))
-		{
-			Sleep(100);
-			printf("→\n");
-		}
-		if (GetAsyncKeyState(VK_DOWN))
-		{
-			Sleep(100);
-			printf("↓\n");
-		}
-	}
+	// 가장 마지막에 있는 NULL 문자를 포함하지 않고
+	// 문자열의 크기를 계산합니다.
+	// const char * string = "Visual";
+
+	//int size = strlen(string);
+
+	// printf("size의 값 : %d", size);
+#pragma endregion
+
+#pragma region 회문
+	// int flag = 1;
+	// 
+	// char * data = "LEVEL";
+	// 
+	// for (int i = 0; i < strlen(data) / 2; i++)
+	// {
+	// 	if (data[i] != data[strlen(data) - 1 - i])
+	// 	{
+	// 		flag = 0;
+	// 		break;
+	// 	}
+	// }
+	// 
+	// if (flag == 1)
+	// {
+	// 	printf("회문\n");
+	// }
+	// else
+	// {
+	// 	printf("회문이 아닙니다.\n");
+	// }
 
 #pragma endregion
+
+
 
 
 	return 0;
