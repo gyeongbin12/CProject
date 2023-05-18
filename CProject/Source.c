01aa999a10e277773b624ae3d976fa1f636f15ad
gyeongbin12 <110909218+gyeongbin12@users.noreply.github.com>
2023-05-18 21:50:56 +0900

    Update Source.c

--- CProject/Source.c
+++ CProject/Source.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h> // ±âº» ÀÔÃâ·Â Çì´õ ÆÄÀÏ	
+#include <stdio.h> // ê¸°ë³¸ ìž…ì¶œë ¥ í—¤ë” íŒŒì¼	
 #include <conio.h>
 #include <Windows.h>
 
@@ -8,10 +8,10 @@
 
 int main()
 {
-#pragma region µ¿Àû ¹è¿­
-	// HEAPÀÌ¶ó´Â ¸Þ¸ð¸® °ø°£À» ÀÌ¿ëÇÏ¿©
-	// ¹è¿­ÀÇ Å©±â¸¦ ÄÄÆÄÀÏ ´Ü°è°¡ ¾Æ´Ñ ½ÇÇà ½Ã°£¿¡
-	// °¡º¯ÀûÀ¸·Î ¹Ù²Ü ¼ö ÀÖ´Â ¹è¿­ÀÔ´Ï´Ù.
+#pragma region ë™ì  ë°°ì—´
+	// HEAPì´ë¼ëŠ” ë©”ëª¨ë¦¬ ê³µê°„ì„ ì´ìš©í•˜ì—¬
+	// ë°°ì—´ì˜ í¬ê¸°ë¥¼ ì»´íŒŒì¼ ë‹¨ê³„ê°€ ì•„ë‹Œ ì‹¤í–‰ ì‹œê°„ì—
+	// ê°€ë³€ì ìœ¼ë¡œ ë°”ê¿€ ìˆ˜ ìžˆëŠ” ë°°ì—´ìž…ë‹ˆë‹¤.
 
 	// int size = 10;
 	// 
@@ -26,14 +26,14 @@ int main()
 	// 
 	// for (int i = 0; i < size; i++)
 	// {
-	// 	printf("arrayPtr[%d]ÀÇ °ª : %d\n", i, arrayPtr[i]);
+	// 	printf("arrayPtr[%d]ì˜ ê°’ : %d\n", i, arrayPtr[i]);
 	// }
 	// 
 	// free(arrayPtr);
 
 #pragma endregion
 
-#pragma region  fflush() ÇÔ¼ö
+#pragma region  fflush() í•¨ìˆ˜
 
 	// char a, b, c;
 	// 
@@ -46,12 +46,12 @@ int main()
 	// scanf_s("%c", &c);
 	// fflush(stdin);
 	// 
-	// printf("a º¯¼öÀÇ °ª : %c\n", a);
-	// printf("b º¯¼öÀÇ °ª : %c\n", b);
-	// printf("c º¯¼öÀÇ °ª : %c\n", c);
+	// printf("a ë³€ìˆ˜ì˜ ê°’ : %c\n", a);
+	// printf("b ë³€ìˆ˜ì˜ ê°’ : %c\n", b);
+	// printf("c ë³€ìˆ˜ì˜ ê°’ : %c\n", c);
 #pragma endregion
 
-#pragma region ¹®ÀÚ¿­ µÚÁý±â
+#pragma region ë¬¸ìžì—´ ë’¤ì§‘ê¸°
 	
 	char string[SIZE] = { 0, };
 
@@ -61,11 +61,6 @@ int main()
 	{
 		printf("%c", string[i]);
 	}
-
-	
-
-
-
 
 #pragma endregion
 
