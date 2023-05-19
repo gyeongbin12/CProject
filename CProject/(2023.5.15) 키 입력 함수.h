2eed9aaa3f345379fbe649c370ebe1a8bec2ace3
kwh0163 <dlawjdgid75@naver.com>
2023-05-19 21:49:07 +0900

    2023.5.19

--- CProject/(2023.5.15) 키 입력 함수.h
+++ CProject/(2023.5.15) 키 입력 함수.h
@@ -16,31 +16,31 @@ int main()
 
 	//while (1)
 	//{
-	//	//if (_kbhit()) // 키보드 입력을 확인하는 함수
-	//	
-	//		key = _getch(); // 방향키 입력시 224 00이 들어오게 되므로 앞에 있는 값 224를 삭제합니다.
+		if (_kbhit()) // 키보드 입력을 확인하는 함수
+		
+			key = _getch(); // 방향키 입력시 224 00이 들어오게 되므로 앞에 있는 값 224를 삭제합니다.
 
-	//		if (key == -32)
-	//		{
-	//			key = _getch();
-	//		}
+			if (key == -32)
+			{
+				key = _getch();
+			}
 
-	//		switch (key)
-	//		{
-	//			case UP: printf("↑\n");
-	//				break;
-	//			case LEFT: printf("←\n");
-	//				break;
-	//			case RIGHT: printf("→\n");
-	//				break;
-	//			case DOWN: printf("↓\n");
-	//				break;
-	//			default: printf("다시 입력해주세요.\n");
-	//				break;
-	//		}
+			switch (key)
+			{
+				case UP: printf("↑\n");
+					break;
+				case LEFT: printf("←\n");
+					break;
+				case RIGHT: printf("→\n");
+					break;
+				case DOWN: printf("↓\n");
+					break;
+				default: printf("다시 입력해주세요.\n");
+					break;
+			}
 
-	//		printf("다른 작업 중...\n");
-	//}
+			printf("다른 작업 중...\n");
+	}
 
 #pragma endregion
 
