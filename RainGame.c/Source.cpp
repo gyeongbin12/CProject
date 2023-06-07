8cc41301a5a78e6adb5e8533220361d52d7b4ce8
kwh0163 <dlawjdgid75@naver.com>
2023-06-07 21:02:45 +0900

    6.7

--- RainGame.c/Source.cpp
+++ RainGame.c/Source.cpp
@@ -1,86 +1,135 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <Windows.h>
-#include <conio.h>	
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define UP 72
 #define LEFT 75
 #define RIGHT 77
-#define DOWN 80
 
-#define WIDTH 20
-#define HEIGHT 10
+#define WIDTH 30
+#define HEIGHT 30
 
 typedef struct Player {
-	int x;
-	int y;
-	const char* shape;
+    int x;
+    int y;
+    const char* shape;
 }Player;
 
-typedef struct Object{
-	int x;
-	int y;
-	char word; 
+typedef struct Object {
+    int x;
+    int y;
+    char word;
 }Object;
 
 void GotoXY(int x, int y)
 {
-	COORD position = { x, y };
+    COORD position = { x, y };
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
 
-void Keyboard(Player* ptrPlayer)
+int RandomX()
 {
-	char key = 0;
+    srand(time(NULL));
 
-	if (_kbhit())
-	{
-		key = _getch();
+    int random = rand() % 28;
 
-		if (key == -32)
-		{
-			key = _getch();
-		}
+    if (random % 2 == 1)
+    {
+        random += 1;
+    }
 
-		switch (key)
-		{
-		case LEFT: if (ptrPlayer->x <= 0) return;
-			ptrPlayer->x -= 2;
-			break;
-		case RIGHT:if (ptrPlayer->x >= 20) return;
-			ptrPlayer->x += 2;
-			break;
-		}
-	}
+    return random;
+}
+
+void Keyboard(Player* ptrPlayer) // 키보드 사용
+{
+    char key = 0;
+
+    if (_kbhit())
+    {
+        key = _getch();
+
+        if (key == -32)
+        {
+            key = _getch();
+        }
+
+        switch (key)
+        {
+        case LEFT: if (ptrPlayer->x <= 0) return;
+            ptrPlayer->x -= 2;
+            break;
+        case RIGHT:if (ptrPlayer->x >= 28) return;
+            ptrPlayer->x += 2;
+            break;
+        }
+    }
 }
 
 int main()
 {
-	Object object = {0,1,'A'};
-	Player player = { 10, 30, "★" };
-	
-	// char a[10] = { "ABCD" };
-	// char b[10] = { "ABCD" };
-	// 두 개의 문자열이 서로 같으면 0을 반환합니다.
-	// 첫 번째 매개변수 문자열의 값이 크다면 1을 반환합니다.
-	// 두 번째 매개변수 문자열의 값이 크다면 -1을 반환합니다.
-	// int value = strcmp(a, b);
-	// printf("value의 값 : %d", value);
-	
-	while (1)
-	{
-		Keyboard(&player);
+    system("mode con cols=30 lines=30");
 
-		GotoXY(player.x, player.y);
-		printf("%s", player.shape);
+    Object object = { RandomX(),3,'A' };
+    Player player = { 10, 28, "★" };
+    
+    int score = 0;
 
-		GotoXY(object.x, object.y++);
-		printf("%c", object.word);
+#pragma region 더미
+    // char a[10] = { "ABCD" };
+    // char b[10] = { "ABCD" };
+    // 두 개의 문자열이 서로 같으면 0을 반환합니다.
+    // 첫 번째 매개변수 문자열의 값이 크다면 1을 반환합니다.
+    // 두 번째 매개변수 문자열의 값이 크다면 -1을 반환합니다.
+    // int value = strcmp(a, b);
 
-		Sleep(1000);
-		system("cls");
-	}
+    // printf("value의 값 : %d", value);
+    int life = 3;
+    int maxscore = 0;
 
-	return 0;
+        // if (score > maxscore)
+        // {
+        //     maxscore = score;
+        // }
+    // printf("Maxscore : %d", maxscore); 
+#pragma endregion
+
+    while (1)
+    {
+        if (object.y>= 29)
+        {
+            object.y = 0;
+            object.x = RandomX();
+        }
+
+        if (player.x == object.x && player.y == object.y)
+        {
+            break;
+        }
+        else
+        {
+            score++;
+        }
+
+        Keyboard(&player);
+
+        GotoXY(player.x, player.y);
+        printf("%s", player.shape);
+
+        GotoXY(object.x, object.y++);
+        printf("%c", object.word);
+
+        Sleep(100);
+        system("cls");
+
+
+        printf("score : %d  ", score);
+        
+    }
+
+    return 0;
 }
